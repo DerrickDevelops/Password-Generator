490bd364de69c51af6bc131e57bb568280a15667
DerrickDevelops <derrickdevelops@gmail.com>
2024-04-03 21:54:45 -0400

    HTML & CSS Changes
    Created Content File to store HTML File.
    
    Added CSS for layout and color changes.

---
+++ src/Content.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './App.css'
+
+const MainContent = () => {
+    return (
+        <div className="wrapper">
+			<div className="heading">
+				<h1>Generate a<br /><span>secure password</span></h1>
+			</div>
+		<div className="subheading">
+			<h2>Have a choice of 2 secure passwords to protect your privacy</h2>
+		</div>
+				<div className="passwordlength">
+				<h3 className="pass_text">Password length</h3>
+			<button id="decrease" onclick="decrease()">-</button>
+			<span id="lengthinput">0</span>
+			<button id="increase" onclick="increase()">+</button>
+		</div>
+		<div className="btn">
+			<button id="button" onclick="generatePass()">Generate password</button>
+			<button id="clear" onclick="clearInputField()">Clear</button>
+		</div>
+
+		<div className="passwords">
+			<input type="text" placeholder="password 1" id="password1" readonly />
+			<input type="text" placeholder="password 2" id="password2" readonly />
+		</div>
+		<div className="copybtn">
+			<button id="copy1" onclick="copyToClipboardOne()">copy</button>
+			<button id="copy2" onclick="copyToClipboardTwo()">copy</button>
+		</div>
+		</div>
+    )
+}
+
+export default MainContent
