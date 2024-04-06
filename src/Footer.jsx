fedbec30c51d5882524ed2e26dbab6d2e7ed0fc8
DerrickDevelops <derrickdevelops@gmail.com>
2024-04-05 22:31:24 -0400

    Updated Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,14 @@
 
 function Footer() {
+
+    const styles = {
+        fontSize: "1.5rem",
+        paddingTop: "1rem"
+    }
+
     return(
         <footer>
-            <div className=""></div>
+            <h3>Made By Derrick Develops</h3>
         </footer>
     )
 }
