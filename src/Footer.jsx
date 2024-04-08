38868d40c5fff7b397092b728f566cf99002c11c
DerrickDevelops <derrickdevelops@gmail.com>
2024-04-07 22:49:01 -0400

    Updated Footer
    Added Contact Links in Footer and styled using Inline styling.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,9 +6,26 @@ function Footer() {
         opacity: "75%"
     }
 
+    const links = {
+        fontSize: "2rem",
+        display: "flex",
+        justifyContent: "center",
+        gap: "2rem"
+    }
+
+    const linkTag = {
+        color: "#213547",
+        opacity: "75%"
+    }
+
     return(
         <footer>
             <h3 style={styles}>Made By Derrick Develops</h3>
+            <div className="contact-links" style={links}>
+                <a href="#" className="git-hub" style={linkTag}><i class="fa-brands fa-github"></i></a>
+                <a href="#" className="linkedin" style={linkTag}><i class="fa-brands fa-linkedin"></i></a>
+                <a href="#" className="personal-website" style={linkTag}><i class="fa-solid fa-globe"></i></a>
+            </div>
         </footer>
     )
 }
