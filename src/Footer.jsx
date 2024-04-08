df69705040ae86e00108aaca5ce84a746feaf2be
DerrickDevelops <derrickdevelops@gmail.com>
2024-04-08 16:30:48 -0400

    Updated Footer
    Added all links to icons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -25,9 +25,9 @@ function Footer() {
         <footer>
             <h3 style={styles}>Made By Derrick Develops</h3>
             <div className="contact-links" style={links}>
-                <a href="#" className="git-hub" style={linkTag}><i class="fa-brands fa-github"></i></a>
-                <a href="#" className="linkedin" style={linkTag}><i class="fa-brands fa-linkedin"></i></a>
-                <a href="#" className="personal-website" style={linkTag}><i class="fa-solid fa-globe"></i></a>
+                <a href="https://github.com/DerrickDevelops/Password-Generator" className="git-hub" style={linkTag}><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/derrick-murray-753a66233" className="linkedin" style={linkTag}><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://derrickdevelops.com/" className="personal-website" style={linkTag}><i class="fa-solid fa-globe"></i></a>
             </div>
         </footer>
     )
